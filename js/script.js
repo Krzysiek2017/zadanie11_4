5fea8c1274d26dced586bc2781cac812fd34efce
logan <musashimiyamoto@interia.eu>
2017-09-07 21:53:10 +0200

    poprawka

--- js/script.js
+++ js/script.js
@@ -9,9 +9,8 @@ Button.prototype = {
 		this.$element.click(function() {
 			alert(self.text);
 		});
-		$('body')
-			.append(this.$element);
+		$('body').append(this.$element);
 	}
-}
+};
 var btn1 = new Button('Hello!');
 btn1.create();
